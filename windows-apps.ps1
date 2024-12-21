2f7ea0c5aca613c5c226d734bf45f452a413da9c
udon247 <57240541+udon247@users.noreply.github.com>
2024-12-22 02:33:43 +0900

    Update windows-apps.ps1

--- windows-apps.ps1
+++ windows-apps.ps1
@@ -1,3 +1,4 @@
 Start-Process 'https://honto.jp/ebook/dlinfo.html'
+Start-Process 'https://support.broadcom.com/group/ecx/productdownloads?subfamily=VMware+Workstation+Pro'
 Start-Process 'https://tablacus.github.io/explorer.html'
 Start-Process 'https://www.amazon.co.jp/gp/browse.html?node=26197586051'
