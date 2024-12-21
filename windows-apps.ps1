ad179059137157ab37efa506ffdeb382ae06fd49
udon247 <57240541+udon247@users.noreply.github.com>
2024-12-22 00:20:40 +0900

    Create windows-apps.ps1

---
+++ windows-apps.ps1
@@ -0,0 +1,3 @@
+Start-Process 'https://honto.jp/ebook/dlinfo.html'
+Start-Process 'https://tablacus.github.io/explorer.html'
+Start-Process 'https://www.amazon.co.jp/gp/browse.html?node=26197586051'
