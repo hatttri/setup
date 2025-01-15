d1b401a82bcd85236c4f04e15ac890c1746affce
hatttori-gh <57240541+hatttori-gh@users.noreply.github.com>
2025-01-15 20:53:01 +0900

    Update

--- dotnet-new-webapi.ps1
+++ dotnet-new-webapi.ps1
@@ -2,6 +2,12 @@
 $solution = "webapi-sln01"
 $project01 = "webapi-project01"
 $project02 = "webapi-project02"
+$language = "C#"
+$framework = "net8.0"
+
+# solution
+dotnet new sln `
+    --output $solution
 
 dotnet new gitignore `
     --output $solution
@@ -9,20 +15,32 @@ dotnet new gitignore `
 dotnet new editorconfig `
     --output $solution
 
-dotnet new sln `
-    --output $solution
-
+# project01
 dotnet new webapi `
     --use-controllers `
-    --language "C#" `
-    --framework "net8.0" `
+    --language $language `
+    --framework $framework `
     --output (Join-Path $solution $project01)
 
-dotnet new webapi `
-    --use-controllers `
-    --language "C#" `
-    --framework "net8.0" `
-    --output (Join-Path $solution $project02)
+dotnet new gitignore `
+    --output (Join-Path $solution $project01)
+
+dotnet new editorconfig `
+    --output (Join-Path $solution $project01)
 
 dotnet sln $solution add (Join-Path $solution $project01)
+
+# project02
+dotnet new webapi `
+    --use-controllers `
+    --language $language `
+    --framework $framework `
+    --output (Join-Path $solution $project02)
+
+dotnet new gitignore `
+    --output (Join-Path $solution $project02)
+
+dotnet new editorconfig `
+    --output (Join-Path $solution $project02)
+
 dotnet sln $solution add (Join-Path $solution $project02)
