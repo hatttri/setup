69186b3b2775ccd1a7f794414cc4d337250ddf33
hatttri <57240541+hatttri@users.noreply.github.com>
2025-04-05 23:41:22 +0900

    Update windows-apps.ps1

--- windows-apps.ps1
+++ windows-apps.ps1
@@ -1,4 +1,3 @@
 Start-Process 'https://honto.jp/ebook/dlinfo.html'
-Start-Process 'https://support.broadcom.com/group/ecx/productdownloads?subfamily=VMware+Workstation+Pro'
 Start-Process 'https://tablacus.github.io/explorer.html'
 Start-Process 'https://www.amazon.co.jp/gp/browse.html?node=26197586051'
