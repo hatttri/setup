7738192d21c9c7d5268163bffb4c1bc22110cfae
hatttri <57240541+hatttri@users.noreply.github.com>
2025-09-02 21:44:50 +0900

    update winget

--- windows-apps.ps1
+++ windows-apps.ps1
@@ -1,3 +1,2 @@
 Start-Process 'https://honto.jp/ebook/dlinfo.html'
 Start-Process 'https://tablacus.github.io/explorer.html'
-Start-Process 'https://www.amazon.co.jp/gp/browse.html?node=26197586051'
